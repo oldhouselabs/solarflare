03bdfd6533e71b4113c12c11ee6cc677362b6f1d
George Seabridge <georgeseabridge@gmail.com>
2024-07-21 10:41:51 -0700

    fix: reflect new API in example app

--- apps/example/app/page.tsx
+++ apps/example/app/page.tsx
@@ -5,11 +5,11 @@ import { createSolarflare, type DB } from "@solarflare/client";
 export const { useTable } = createSolarflare<DB>();
 
 export default function Home() {
-  const { loading, data } = useTable("employees", {
+  const { isLoading, data } = useTable("employees", {
     sort: (a, b) => a.name.localeCompare(b.name),
   });
 
-  if (loading) {
+  if (isLoading) {
     return <div>Loading</div>;
   }
 
