843322946e09b6610665c108a4494b8a058ae641
George Seabridge <georgeseabridge@gmail.com>
2024-07-25 16:02:09 -0700

    chore: reorg example app

---
+++ apps/example/app/components/employees.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { createSolarflare, type DB } from "@solarflare/client";
+
+const { useTable } = createSolarflare<DB>();
+
+export const Employees = () => {
+  const { isLoading, data, optimistic } = useTable("employees", {
+    sort: (a, b) => a.name.localeCompare(b.name),
+  });
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  const handleClick = () => {
+    // Pretend you make a network request here.
+    // myNetworkRequest()
+
+    // Optimistically update the UI so we don't wait for the network request
+    // to see the change.
+    const { rollback } = optimistic({
+      action: "update",
+      uuid: 10185,
+      data: { name: "Alice" },
+    });
+
+    // Pretend the network request comes back with some kind of rejection here.
+    setTimeout(() => rollback(), 2000);
+  };
+
+  return (
+    <div>
+      <ul>
+        {data.map((employee) => (
+          <li key={employee.uuid}>{employee.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
+};
